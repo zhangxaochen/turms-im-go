1aa9b9991d1e0f3070069ee19a308c4880d71d00
zhangxaochen <zhangxaochen@gmail.com>
2026-04-02 21:36:02 +0800

    feat(common): add sharded_map, constants, and mongo_utils

---
+++ internal/domain/common/cache/sharded_map.go
@@ -0,0 +1,74 @@
+package cache
+
+import (
+	"sync"
+)
+
+// ShardedMap is a concurrent-safe map that uses sharding to reduce lock contention.
+type ShardedMap[K comparable, V any] struct {
+	shards []*shard[K, V]
+	num    uint32
+	hasher func(K) uint32
+}
+
+type shard[K comparable, V any] struct {
+	sync.RWMutex
+	m map[K]V
+}
+
+// NewShardedMap creates a new ShardedMap with the given number of shards and a hasher.
+func NewShardedMap[K comparable, V any](numShards uint32, hasher func(K) uint32) *ShardedMap[K, V] {
+	s := &ShardedMap[K, V]{
+		shards: make([]*shard[K, V], numShards),
+		num:    numShards,
+		hasher: hasher,
+	}
+	for i := uint32(0); i < numShards; i++ {
+		s.shards[i] = &shard[K, V]{
+			m: make(map[K]V),
+		}
+	}
+	return s
+}
+
+// NewStringShardedMap is a convenience function for string keys.
+func NewStringShardedMap[V any](numShards uint32) *ShardedMap[string, V] {
+	return NewShardedMap[string, V](numShards, Fnv32)
+}
+
+func (s *ShardedMap[K, V]) getShard(key K) *shard[K, V] {
+	return s.shards[s.hasher(key)%s.num]
+}
+
+func (s *ShardedMap[K, V]) Set(key K, value V) {
+	shard := s.getShard(key)
+	shard.Lock()
+	shard.m[key] = value
+	shard.Unlock()
+}
+
+func (s *ShardedMap[K, V]) Get(key K) (V, bool) {
+	shard := s.getShard(key)
+	shard.RLock()
+	val, ok := shard.m[key]
+	shard.RUnlock()
+	return val, ok
+}
+
+func (s *ShardedMap[K, V]) Delete(key K) {
+	shard := s.getShard(key)
+	shard.Lock()
+	delete(shard.m, key)
+	shard.Unlock()
+}
+
+// Fnv32 is a simple hash function for strings.
+func Fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
---
+++ internal/domain/common/constant/status_code.go
@@ -0,0 +1,93 @@
+package constant
+
+// ResponseStatusCode defines the business response codes for Turms.
+type ResponseStatusCode int32
+
+const (
+	ResponseStatusCode_OK ResponseStatusCode = 1000
+
+	// Client Error
+	ResponseStatusCode_INVALID_REQUEST  ResponseStatusCode = 1100
+	ResponseStatusCode_ILLEGAL_ARGUMENT ResponseStatusCode = 1102
+
+	// Server Error
+	ResponseStatusCode_SERVER_INTERNAL_ERROR ResponseStatusCode = 1200
+
+	// Group Error
+	ResponseStatusCode_UPDATE_INFO_OF_NONEXISTENT_GROUP           ResponseStatusCode = 3000
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_UPDATE_GROUP_INFO       ResponseStatusCode = 3001
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_UPDATE_GROUP_INFO ResponseStatusCode = 3002
+	ResponseStatusCode_NOT_GROUP_MEMBER_TO_UPDATE_GROUP_INFO      ResponseStatusCode = 3003
+
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_TRANSFER_GROUP ResponseStatusCode = 3201
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_DELETE_GROUP   ResponseStatusCode = 3202
+	ResponseStatusCode_GROUP_SUCCESSOR_NOT_GROUP_MEMBER  ResponseStatusCode = 3203
+	ResponseStatusCode_MAX_OWNED_GROUPS_REACHED           ResponseStatusCode = 3205
+	ResponseStatusCode_TRANSFER_NONEXISTENT_GROUP        ResponseStatusCode = 3206
+
+	ResponseStatusCode_ADD_USER_TO_GROUP_REQUIRING_USERS_APPROVAL ResponseStatusCode = 3400
+	ResponseStatusCode_ADD_USER_TO_INACTIVE_GROUP              ResponseStatusCode = 3401
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_ADD_GROUP_MANAGER     ResponseStatusCode = 3402
+	ResponseStatusCode_ADD_USER_TO_GROUP_WITH_SIZE_LIMIT_REACHED ResponseStatusCode = 3403
+	ResponseStatusCode_ADD_BLOCKED_USER_TO_GROUP                 ResponseStatusCode = 3404
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_ADD_GROUP_MEMBER       ResponseStatusCode = 3405
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_ADD_GROUP_MEMBER ResponseStatusCode = 3406
+	ResponseStatusCode_NOT_GROUP_MEMBER_TO_ADD_GROUP_MEMBER      ResponseStatusCode = 3407
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_REMOVE_GROUP_MEMBER ResponseStatusCode = 3408
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_REMOVE_GROUP_OWNER_OR_MANAGER ResponseStatusCode = 3409
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_UPDATE_GROUP_MEMBER_ROLE ResponseStatusCode = 3410
+	ResponseStatusCode_UPDATE_GROUP_MEMBER_ROLE_OF_NONEXISTENT_GROUP ResponseStatusCode = 3411
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_UPDATE_GROUP_MEMBER_INFO ResponseStatusCode = 3412
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_UPDATE_GROUP_MEMBER_INFO ResponseStatusCode = 3413
+	ResponseStatusCode_NOT_GROUP_MEMBER_TO_UPDATE_GROUP_MEMBER_INFO ResponseStatusCode = 3414
+	ResponseStatusCode_UPDATE_GROUP_MEMBER_INFO_OF_NONEXISTENT_GROUP ResponseStatusCode = 3415
+	ResponseStatusCode_UPDATE_INFO_OF_NONEXISTENT_GROUP_MEMBER      ResponseStatusCode = 3416
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_MUTE_GROUP_MEMBER ResponseStatusCode = 3417
+	ResponseStatusCode_MUTE_GROUP_MEMBER_WITH_ROLE_EQUAL_TO_OR_HIGHER_THAN_REQUESTER ResponseStatusCode = 3418
+	ResponseStatusCode_MUTE_GROUP_MEMBER_OF_NONEXISTENT_GROUP ResponseStatusCode = 3419
+	ResponseStatusCode_MUTE_NONEXISTENT_GROUP_MEMBER          ResponseStatusCode = 3420
+	ResponseStatusCode_NOT_GROUP_MEMBER_TO_QUERY_GROUP_MEMBER_INFO ResponseStatusCode = 3421
+	ResponseStatusCode_USER_JOIN_GROUP_WITHOUT_ACCEPTING_GROUP_INVITATION ResponseStatusCode = 3422
+	ResponseStatusCode_USER_JOIN_GROUP_WITHOUT_ANSWERING_GROUP_QUESTION ResponseStatusCode = 3423
+	ResponseStatusCode_USER_JOIN_GROUP_WITHOUT_SENDING_GROUP_JOIN_REQUEST ResponseStatusCode = 3424
+
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_ADD_BLOCKED_USER    ResponseStatusCode = 3500
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_REMOVE_BLOCKED_USER ResponseStatusCode = 3501
+
+	// Group - Join Request
+	ResponseStatusCode_GROUP_JOIN_REQUEST_IS_DISABLED           ResponseStatusCode = 3600
+	ResponseStatusCode_ADD_USER_TO_GROUP_REQUIRING_INVITATION     ResponseStatusCode = 3601
+	ResponseStatusCode_RECALLING_GROUP_JOIN_REQUEST_IS_DISABLED    ResponseStatusCode = 3602
+	ResponseStatusCode_RECALL_NON_PENDING_GROUP_JOIN_REQUEST       ResponseStatusCode = 3603
+	ResponseStatusCode_NOT_SENDER_TO_RECALL_GROUP_JOIN_REQUEST    ResponseStatusCode = 3604
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_QUERY_GROUP_JOIN_REQUEST ResponseStatusCode = 3605
+	ResponseStatusCode_UPDATE_NON_PENDING_GROUP_JOIN_REQUEST       ResponseStatusCode = 3606
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_UPDATE_GROUP_JOIN_REQUEST ResponseStatusCode = 3607
+
+	// Group - Invitation
+	ResponseStatusCode_SENDING_GROUP_INVITATION_IS_DISABLED       ResponseStatusCode = 3700
+	ResponseStatusCode_QUERY_GROUP_INVITATIONS_IS_DISABLED         ResponseStatusCode = 3701
+	ResponseStatusCode_NOT_GROUP_OWNER_TO_SEND_GROUP_INVITATION     ResponseStatusCode = 3702
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_SEND_GROUP_INVITATION ResponseStatusCode = 3703
+	ResponseStatusCode_NOT_GROUP_MEMBER_TO_SEND_GROUP_INVITATION    ResponseStatusCode = 3704
+	ResponseStatusCode_RECALLING_GROUP_INVITATION_IS_DISABLED      ResponseStatusCode = 3705
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_RECALL_GROUP_INVITATION ResponseStatusCode = 3706
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_OR_SENDER_TO_RECALL_GROUP_INVITATION ResponseStatusCode = 3707
+	ResponseStatusCode_RECALL_NON_PENDING_GROUP_INVITATION          ResponseStatusCode = 3708
+	ResponseStatusCode_UPDATE_NON_PENDING_GROUP_INVITATION          ResponseStatusCode = 3709
+	ResponseStatusCode_NOT_INVITEE_TO_UPDATE_GROUP_INVITATION       ResponseStatusCode = 3710
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_QUERY_GROUP_INVITATION ResponseStatusCode = 3711
+
+	// Group - Question
+	ResponseStatusCode_QUERY_GROUP_JOIN_QUESTIONS_IS_DISABLED      ResponseStatusCode = 3800
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_CREATE_GROUP_QUESTION ResponseStatusCode = 3801
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_DELETE_GROUP_QUESTION ResponseStatusCode = 3802
+	ResponseStatusCode_NOT_GROUP_OWNER_OR_MANAGER_TO_UPDATE_GROUP_QUESTION ResponseStatusCode = 3803
+	ResponseStatusCode_CHECKING_GROUP_JOIN_QUESTION_ANSWER_IS_DISABLED ResponseStatusCode = 3804
+	ResponseStatusCode_ANSWER_GROUP_QUESTION_IS_DISABLED           ResponseStatusCode = 3805
+	ResponseStatusCode_ANY_GROUP_QUESTION_ANSWER_IS_INCORRECT       ResponseStatusCode = 3806
+
+	ResponseStatusCode_BLOCKED_USER_SEND_GROUP_JOIN_REQUEST         ResponseStatusCode = 3404
+	ResponseStatusCode_USER_ALREADY_GROUP_MEMBER                       ResponseStatusCode = 3405
+	ResponseStatusCode_GROUP_JOIN_STRATEGY_NOT_JOIN_REQUEST         ResponseStatusCode = 3407
+)
